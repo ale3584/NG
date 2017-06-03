1384bfae10354f3277762a55e643fdcdf8a544b0
ALEXANDER <a.petrovskii@gmail.com>
2017-06-03 22:18:05 +0300

    setup components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe',
+      ' This is a test',
+      'http://www.seriouseats.com/recipes/assets_c/2016/06/20160623-cubano-roast-pork-sandwich-recipe-19-thumb-1500xauto-432791.jpg'),
+    new Recipe('A Test Recipe 2',
+      ' This is a test',
+      'http://www.seriouseats.com/recipes/assets_c/2016/06/20160623-cubano-roast-pork-sandwich-recipe-19-thumb-1500xauto-432791.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
